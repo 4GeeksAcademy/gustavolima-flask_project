c562c55f4f7a178b2b66961ab1444072cb2977ca
Gustavo Lima <gustavoblima@me.com>
2023-08-11 19:14:50 +0000

    First Update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Template for Machine Learning projects
+Flask App 
--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+with open("../models/random_forest_classifier_default_42.sav", 'rb') as f:
+    model = load(f)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        
+        Pregnancies = float(request.form["val1"])
+        Glucose = float(request.form["val2"])
+        BloodPressure = float(request.form["val3"])
+        BMI = float(request.form["val4"])
+        DiabetesPedigreeFunction = float(request.form["val5"])
+        Age = float(request.form["val6"])
+        
+        data = [[Pregnancies, Glucose, BloodPressure, BMI, DiabetesPedigreeFunction, Age]]
+        prediction = model.predict(data)
+    
+    return render_template("index.html", prediction = prediction)
